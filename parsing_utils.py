aa9003413b4ef504003f1cdf896b6f48e8c4b486
EVHEN KOROLOV <fricker12@gmail.com>
2023-05-23 16:49:25 +0300

    add code

---
+++ parsing_utils.py
@@ -0,0 +1,146 @@
+import csv
+import re
+import json
+import argparse
+
+def import_data(file_path, delimiter=',', has_header=True):
+    with open(file_path, 'r') as file:
+        if delimiter == 'tsv':
+            delimiter = '\t'
+        elif delimiter == 'dsv':
+            delimiter = ';'
+
+        reader = csv.reader(file, delimiter=delimiter)
+        data = list(reader)
+        if has_header:
+            header = data[0]
+            data = data[1:]
+            return header, data
+        else:
+            return None, data
+
+def export_data(file_path, data, delimiter=',', header=None):
+    with open(file_path, 'w', newline='') as file:
+        if delimiter == 'tsv':
+            delimiter = '\t'
+        elif delimiter == 'dsv':
+            delimiter = ';'
+
+        writer = csv.writer(file, delimiter=delimiter)
+        if header:
+            writer.writerow(header)
+        writer.writerows(data)
+
+def filter_data(data, column, value):
+    filtered_data = []
+    for row in data:
+        if row[column] == value:
+            filtered_data.append(row)
+    return filtered_data
+
+def filter_data_regex(data, column, pattern):
+    filtered_data = []
+    regex = re.compile(pattern)
+    for row in data:
+        if regex.search(row[column]):
+            filtered_data.append(row)
+    return filtered_data
+
+def filter_data_multiple_regex(data, columns, patterns):
+    filtered_data = []
+    regexes = [re.compile(pattern) for pattern in patterns]
+    for row in data:
+        if all(regex.search(row[column]) for column, regex in zip(columns, regexes)):
+            filtered_data.append(row)
+    return filtered_data
+
+def extract_rows(data, ranges):
+    extracted_data = []
+    for start, end in ranges:
+        extracted_data.extend(data[start-1:end])
+    return extracted_data
+
+def extract_columns(data, columns):
+    extracted_data = []
+    for row in data:
+        extracted_data.append([row[column] for column in columns])
+    return extracted_data
+
+def process_file(file_path):
+    try:
+        header, data = import_data(file_path)
+        json_data = json.dumps(data)
+        print(json_data)
+    except csv.Error as e:
+        print(f"CSV format error: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description='Utils for parsing files')
+    parser.add_argument('file_path', help='Path to the CSV file')
+    parser.add_argument('--delimiter', choices=['comma', 'tab', 'semicolon'], default='comma',
+                        help='Delimiter used in the CSV file (default: comma)')
+    parser.add_argument('--header', action='store_true', help='Specify if the CSV file has a header row')
+    parser.add_argument('--filter', nargs=3, metavar=('column', 'value', 'output_file'),
+                        help='Filter data by a specific value in a column and save the result to a file')
+    parser.add_argument('--regex-filter', nargs=3, metavar=('column', 'pattern', 'output_file'),
+                        help='Filter data by a regular expression pattern in a column and save the result to a file')
+    parser.add_argument('--multi-regex-filter', nargs='*', metavar=('column', 'pattern'),
+                        help='Filter data by multiple regular expression patterns in multiple columns')
+    parser.add_argument('--extract-rows', nargs='+', type=int, metavar=('start', 'end'),
+                        help='Extract rows from the specified start to end indices and print the result')
+    parser.add_argument('--extract-columns', nargs='+', type=int, metavar='column',
+                        help='Extract specified columns and print the result')
+    parser.add_argument('--export', metavar='output_file', help='Export data to a CSV file')
+
+    args = parser.parse_args()
+
+    file_path = args.file_path
+    delimiter = ','
+    if args.delimiter == 'tab':
+        delimiter = 'tsv'
+    elif args.delimiter == 'semicolon':
+        delimiter = 'dsv'
+    has_header = args.header
+
+    header, data = import_data(file_path, delimiter=delimiter, has_header=has_header)
+
+    if args.filter:
+        column, value, output_file = args.filter
+        # Фильтрация по значению в определенном поле
+        filtered_data = filter_data(data, int(column), value)
+        export_data(output_file, filtered_data, delimiter=delimiter, header=header)
+    
+    if args.regex_filter:
+        column, pattern, output_file = args.regex_filter
+        # Фильтрация по регулярному выражению в определенном поле
+        filtered_data_regex = filter_data_regex(data, int(column), pattern)
+        export_data(output_file, filtered_data_regex, delimiter=delimiter, header=header)
+
+    if args.multi_regex_filter:
+        columns = [int(column) for column in args.multi_regex_filter[::2]]
+        patterns = args.multi_regex_filter[1::2]
+        # Фильтрация по регулярным выражениям в нескольких полях
+        filtered_data_multiple_regex = filter_data_multiple_regex(data, columns, patterns)
+        export_data('output.csv', filtered_data_multiple_regex, delimiter=delimiter, header=header)
+    
+    if args.extract_rows:
+        ranges = [(start, end) for start, end in zip(args.extract_rows[::2], args.extract_rows[1::2])]
+        # Обработка фрагмента файла: извлечение строк по диапазонам
+        extracted_rows = extract_rows(data, ranges)
+        print(extracted_rows)
+    
+    if args.extract_columns:
+        # Обработка фрагмента файла: извлечение столбцов
+        extracted_columns = extract_columns(data, args.extract_columns)
+        print(extracted_columns)
+
+    if args.export:
+        # Экспорт данных в CSV
+        export_data(args.export, data, delimiter=delimiter, header=header)
+
+    # Преобразование CSV в JSON
+    json_data = json.dumps(data)
+    print(json_data)
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Utils for parsing files
+
+## Requirements
+
+You need the followin to be able to run this code:
+
+
+
+## Usage
+
+First install the script and it's requirements:
+
+```
+git clone https://github.com/fricker12/File-parsing-utility
+cd File-parsing-utility
+
+```
+Then run the script as follows:
+```
+Фильтрация по значению в определенном поле:
+python parsing_utils.py input.csv --filter 2 value output.csv
+Эта команда фильтрует данные из input.csv по значению 'value' во втором столбце и сохраняет результат в output.csv.
+
+Фильтрация по регулярному выражению в определенном поле:
+python parsing_utils.py input.csv --regex-filter 1 pattern output.csv
+Эта команда фильтрует данные из input.csv с использованием регулярного выражения 'pattern' (например pattern=r'\d{4}') в первом столбце и сохраняет результат в output.csv.
+
+Фильтрация по регулярным выражениям в нескольких полях:
+python parsing_utils.py input.csv --multi-regex-filter 0 pattern1 2 pattern2
+Эта команда фильтрует данные из input.csv с использованием регулярных выражений 'pattern1' в нулевом столбце и 'pattern2' (например pattern2 = r'example', r'\d{4}') во втором столбце.
+
+Обработка фрагмента файла: извлечение строк по диапазонам:
+python parsing_utils.py input.csv --extract-rows 2 4 7 9
+Эта команда извлекает строки из input.csv с 2 по 4 и с 7 по 9 (включительно) и выводит результат на экран.
+
+Обработка фрагмента файла: извлечение столбцов:
+python parsing_utils.py input.csv --extract-columns 1 3 5
+Эта команда извлекает столбцы 1, 3 и 5 из input.csv и выводит результат на экран.
+
+Экспорт данных в CSV:
+python parsing_utils.py input.csv --export output.csv
+Эта команда экспортирует данные из input.csv в output.csv.
+
+Преобразование CSV в JSON:
+python parsing_utils.py input.csv
+Эта команда преобразует данные из input.csv в формат JSON и выводит результат на экран.
+
+
+
